2be4dcb44f1866fad1c52661eed72717a9c957dd
Kalyan Gokavarapu <gokavarapu.kalyan@hotmail.com>
2020-04-06 19:07:58 +0530

    Initial template for DependencyInjection

---
+++ DependencyInjection/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using WithDependencyInjection;
+using WithoutDependencyInjection;
+
+namespace DependencyInjection
+{
+  class Program
+  {
+    public static void Main(string[] args)
+    {
+      WithDI DI = new WithDI();
+      DI.Run();
+
+      WithoutDI NoDI = new WithoutDI();
+      NoDI.Run();
+    }
+  }
+}
---
+++ DependencyInjection/WithDI.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Reflection;
+
+namespace WithDependencyInjection
+{
+  class WithDI
+  {
+    public void Run()
+    {
+      UserInterface UI = new UserInterface();
+      UI.render();
+      Console.Read();
+    }
+  }
+
+  public class UserInterface
+  {
+    public void render()
+    {
+      var method = MethodBase.GetCurrentMethod();
+      Console.Write("Please enter your Username:");
+      var username = Console.ReadLine();
+      Console.Write("Please enter the password:");
+      var password = Console.ReadLine();
+      Business BL = new Business();
+      Console.WriteLine("UI: Done collecting Username & Password :{0}.{1}", method.ReflectedType.Name,method.Name);
+      BL.signUp(username, password);      
+    }   
+    
+  }
+
+  public class Business
+  {
+    public void signUp(string userName, string Password)
+    {
+      var method = MethodBase.GetCurrentMethod();
+      //Validation
+      DataAccess DL = new DataAccess();
+      Console.WriteLine("BL: Done Validating the User :{0}.{1}", method.ReflectedType.Name, method.Name);
+      DL.store(userName, Password);
+    }
+  }
+
+  public class DataAccess
+  {
+    // Insert/Update DB
+    internal void store(string userName, string password)
+    {
+      var method = MethodBase.GetCurrentMethod();
+      Console.WriteLine("DA: Done Updating the SQL db :{0}.{1}", method.ReflectedType.Name, method.Name);
+    }
+  }
+}
